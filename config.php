2f0b79d4b5b856e28b050cad71aff140cbccbd26
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-11-21 00:11:02 +0600

    PHP Data Read Operation

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+    $connection = new mysqli ('localhost', 'root', '', 'school_management_system_2'); //true, false
+    
+    // if($connection){
+    //     echo "Successfully Connected";
+    // }
+
+    // else{
+    //     echo "Connection Failed";
+    // }
+?>
